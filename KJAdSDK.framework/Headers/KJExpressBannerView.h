58df5bd40a7a984169599aa7100e81cd112de1fc
clh <994060664@qq.com>
2021-07-12 17:33:06 +0800

    banner轮播设置开放

--- KJAdSDK.framework/Headers/KJExpressBannerView.h
+++ KJAdSDK.framework/Headers/KJExpressBannerView.h
@@ -21,6 +21,8 @@ NS_ASSUME_NONNULL_BEGIN
 // SDK版本
 @property(nonatomic, readonly) NSString *Version;
 
+@property (nonatomic, assign, readonly) NSInteger interval;
+
 /**
 *  构造方法
 *  详解：frame - banner
@@ -33,6 +35,22 @@ NS_ASSUME_NONNULL_BEGIN
                     SlotID:(NSString *)slotID
             viewController:(UIViewController *)viewController
                         adSize:(CGSize)adsize;
+
+/**
+*  构造方法
+*  详解：frame - banner
+*       adSize - adSize
+*       AppID - 媒体 ID
+*       SlotID - 广告位 ID
+*       viewController - 视图控制器
+*......interval - 广告刷新间隔，范围 [30, 120] 秒，0秒为不设置轮播
+*/
+- (instancetype)initWithAppID:(NSString *)AppID
+                    SlotID:(NSString *)slotID
+            viewController:(UIViewController *)viewController
+                       adSize:(CGSize)adsize
+                     interval:(NSInteger)interval;
+
 
 // 拉取并展示广告
 - (void)loadAdAndShow;
